a5679677a8e725f6d80a278ee0184903653b5807
zhaojipeng <zhaojipeng@haodai.com>
2017-09-30 18:39:57 +0800

    添加composer autoload

--- src/Login.php
+++ src/Login.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace WechatRobot;
+
+use PHPQRCode\QRcode;
 /**
  * Created by PhpStorm.
  * User: zhaojipeng
@@ -23,8 +26,22 @@ class Login
 
         $operation = 'jslogin';
 
+        $url = $this->url . $operation;
+
+        $response = http_get($url, $params);
+
+        if ($response === false) {
+            return false;
+        }
+
+        $data = parsing_point($response);
+
+        if ($data['code'] != 200) {
+            return false;
+        }
+
         //调用请求
-        $this->uuid = get();
+        $this->uuid = $data['uuid'];
 
         return $this->uuid;
     }
@@ -32,28 +49,40 @@ class Login
     public function getQrCode()
     {
         $operation = 'qrcode';
-        $url = $this->url . $operation . '/' . $uuid;
+        $url = $this->url . $operation . '/' . $this->uuid;
 
         $qrCode = get($url);
 
         return $qrCode;
     }
 
-    public function listenScan($uuid)
+    public function getQrCodeByCli()
     {
-        $operation = 'cgi-bin/mmwebwx-bin/login';
+        $qrcode_val = $this->url.'l/'.$this->uuid;
+        return QRcode::text($qrcode_val);
+    }
 
-        $params = [
-            'loginicon' => 'true',
-            'uuid' => $uuid,
-            'tip' => '0',
-            'r' => '-1160587432',
-            '_' => '1452859503803',
-        ];
+    public function listenScan()
+    {
+        $this->listenEvnet();
     }
 
     public function listenClick()
     {
+        $this->listenEvnet();
+    }
 
+    private function listenEvnet()
+    {
+        set_time_limit(0);
+        $operation = 'cgi-bin/mmwebwx-bin/login';
+
+        $params = [
+            'loginicon' => 'true',
+            'uuid' => $this->uuid,
+            'tip' => '0',
+            'r' => '-1160587432',
+            '_' => '1452859503803',
+        ];
     }
 }
--- src/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: zhaojipeng
- * Date: 17/9/29
- * Time: 14:23
- */
-
